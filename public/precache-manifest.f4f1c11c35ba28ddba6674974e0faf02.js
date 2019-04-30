906b15926b2788371ec0b4d4fe25870b3e449798
Bar Levin <barlevinn@gmail.com>
2019-04-30 20:04:55 +0300

    vacations project

---
+++ public/precache-manifest.f4f1c11c35ba28ddba6674974e0faf02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f6cd0e90e29225f8f210",
+    "url": "/static/js/main.f6cd0e90.chunk.js"
+  },
+  {
+    "revision": "3d2f377180f561d5f1f0",
+    "url": "/static/js/2.3d2f3771.chunk.js"
+  },
+  {
+    "revision": "f6cd0e90e29225f8f210",
+    "url": "/static/css/main.76afeabc.chunk.css"
+  },
+  {
+    "revision": "f9a368578e1fa0ca6b5a9ee883e60a4e",
+    "url": "/index.html"
+  }
+];
